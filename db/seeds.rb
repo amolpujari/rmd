2a4b320dd55033a3d18b5016166ac49bcd80a3c6
Amol Pujari <amolpujari@gmail.com>
2016-10-04 21:52:59 +0530

    make seed quick

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Category.destroy_all
 puts 'seeding...'
 
 t1 = Time.now
-1.upto(1000000).each do |i|
+1.upto(10000).each do |i|
 
   product_name = Faker::Commerce.product_name
   p = Product.create name: "#{product_name} #{Time.now.to_i.to_s[7..-1]}", price: Faker::Commerce.price, description: Faker::Hipster.paragraph rescue nil
@@ -18,7 +18,7 @@ t1 = Time.now
   end
 
   if Time.now-t1>10
-    puts "#{Product.count} unique products.... trying to add #{1000000 - i} more..."
+    puts "#{Product.count} unique products.... trying to add #{10000 - i} more..."
     t1 = Time.now
   end
 end
